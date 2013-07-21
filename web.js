9787a4991db44e60ed8362fa80ffd1e3852387e6
hbothra <hb4100@gmail.com>
2013-07-21 13:01:30 +0530

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
+var fileBuffer = fs.readFileSync('hw3.txt');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(fileBuffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.port || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("LISTENING ON " + port);
 });
