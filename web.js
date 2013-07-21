c2ac89f385e166e0182b2003680a7a901b7e6876
hbothra <hb4100@gmail.com>
2013-07-21 13:06:20 +0530

    Updated web.js

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
-var fileBuffer = fs.readFileSync('hw3.txt');
+var fileBuffer = fs.readFileSync('index.html');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
